12d2f6da0207dc4676c2e3a57f49a24723be23c5
Zahra Mammadli <zahra.mrj@gmail.com>
2022-02-10 23:24:02 +0100

    Create controller for all Users

---
+++ controllers/userController.js
@@ -0,0 +1,14 @@
+const { User } = require("../models");
+
+module.exports = {
+  // Get all Users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      console.log(err);
+      res.status(400).json(err);
+    }
+  },
+};
