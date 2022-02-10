f9b95a6538a1e89a2d0d5208559331277fe54e69
Zahra Mammadli <zahra.mrj@gmail.com>
2022-02-10 22:50:46 +0100

    Prepare sample data for seeding

---
+++ utils/data.js
@@ -0,0 +1,14 @@
+const userData = [
+  {
+    username: "Zahra Mammadli",
+    email: "zahra.mrj@gmail.com",
+  },
+];
+
+const thoughtData = [
+  {
+    thoughtText: "this is a sample text to test",
+  },
+];
+
+module.exports = { userData, thoughtData };
